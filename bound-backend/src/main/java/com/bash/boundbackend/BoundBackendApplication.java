a51450b789ca14463a4e308b16d9ff8557f1d0db
Bashimane-Mamabolo <mamabolobashimane@gmail.com>
2025-10-13 00:25:10 +0200

    Implemented user entity & spring security userdetails

--- bound-backend/src/main/java/com/bash/boundbackend/BoundBackendApplication.java
+++ bound-backend/src/main/java/com/bash/boundbackend/BoundBackendApplication.java
@@ -2,8 +2,10 @@ package com.bash.boundbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BoundBackendApplication {
 
     public static void main(String[] args) {
---
+++ bound-backend/src/main/java/com/bash/boundbackend/entity/user/User.java
@@ -0,0 +1,79 @@
+package com.bash.boundbackend.entity.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Principal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "_user")
+@EntityListeners(AuditingEntityListener.class) // tracks when user entity was created and updated
+public class User implements UserDetails, Principal {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private LocalDate dateOfBirth;
+    @Column(unique = true) //Unique identifier of our user
+    private String email;
+    private String password;
+    private boolean accountLocked;
+    private boolean enabled;
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime modifiedAt;
+
+    //List of roles with Get Authorities
+
+
+
+
+
+    @Override
+    public String getName() {
+        return email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return  enabled;
+    }
+
+    private String getFullName(){
+        return firstname + " " + lastname;
+    }
+}
