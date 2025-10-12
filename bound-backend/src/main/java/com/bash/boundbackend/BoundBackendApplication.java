b3126de44200bd184fd4258a385c1e953f59f709
Bashimane-Mamabolo <mamabolobashimane@gmail.com>
2025-10-12 18:44:58 +0200

    setup sb project and dependencies

---
+++ bound-backend/src/main/java/com/bash/boundbackend/BoundBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bash.boundbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoundBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoundBackendApplication.class, args);
+    }
+
+}
---
+++ bound-backend/src/test/java/com/bash/boundbackend/BoundBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bash.boundbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoundBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
