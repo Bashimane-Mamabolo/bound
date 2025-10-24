26df11587580145db9fd4325e77eb9ff82e87958
Bashimane Mamabolo <117764630+Bashimane-Mamabolo@users.noreply.github.com>
2025-10-25 00:42:25 +0200

    implemented login page, configured routes

--- bound-frontend/src/app/pages/login/login.component.ts
+++ bound-frontend/src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserAuthenticationRequest } from "src/app/services/models/user-authentication-request";
+import { Router } from "@angular/router";
+import { AuthenticationControllerService } from "src/app/services/services/authentication-controller.service";
 
 @Component({
   selector: 'app-login',
@@ -7,4 +10,44 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  authRequest: UserAuthenticationRequest = {
+    email: '',
+    password: ''
+  };
+  errorMessage: Array<string> = [];
+
+  constructor(
+    private router : Router,
+    private authService: AuthenticationControllerService
+    // another service
+  ) {
+  }
+
+  login() {
+    this.errorMessage = [];
+    this.authService.authenticateUser({
+      body: this.authRequest
+    }).subscribe({
+      next: (response) => {
+        // TODO: Handle successful authentication (e.g., store token, redirect)
+        this.router.navigate(['books'])  //route the user to books page after successful login
+      },
+      error: (error) => {
+        console.log(error);
+        if(error.error.validationErrors){
+          this.errorMessage = error.error.validationErrors;
+
+        } else {
+          this.errorMessage.push(error.error.error);
+        }
+
+      }
+    })
+
+  }
+
+  Register() {
+
+    this.router.navigate(['register']);
+  }
 }
