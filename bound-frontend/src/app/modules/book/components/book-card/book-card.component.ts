8dbe8a6cfceebe4e54dde850a5e67272faa9d59a
Bashimane Mamabolo <117764630+Bashimane-Mamabolo@users.noreply.github.com>
2025-11-06 23:58:31 +0200

    finalized book-card comp with action btns

--- bound-frontend/src/app/modules/book/components/book-card/book-card.component.ts
+++ bound-frontend/src/app/modules/book/components/book-card/book-card.component.ts
@@ -7,7 +7,6 @@ import { BookResponse } from "src/app/services/models/book-response";
   styleUrls: ['./book-card.component.scss']
 })
 export class BookCardComponent {
-
   private _book: BookResponse = {};
 
   get book(): BookResponse {
@@ -17,6 +16,28 @@ export class BookCardComponent {
   @Input()
   set book(value){
     this._book = value;
+  }
+
+  private _bookCover: string | undefined;
+
+  get bookCover(): string | undefined {
+    if (this._book.bookCover){
+      return 'data:image/jpg;base64, ' + this._book.bookCover;
+    }
+    // return this._bookCover;
+    // return 'https://source.unsplash.com/user/c_v_r/1900x800' unsplash not working
+    return 'https://picsum.photos/200/300';
+  }
+
+  private _manage = false;
+
+  get manage(): boolean {
+    return this._manage;
+  }
+
+  @Input()
+  set manage(value: boolean) {
+    this._manage = value;
   }
 
 }
