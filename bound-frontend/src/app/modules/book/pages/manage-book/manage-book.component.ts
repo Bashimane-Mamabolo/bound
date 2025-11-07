95c2dabe1711eee6298a4a95b432716de999e2ba
Bashimane Mamabolo <117764630+Bashimane-Mamabolo@users.noreply.github.com>
2025-11-07 23:40:56 +0200

    impl edit book feature

--- bound-frontend/src/app/modules/book/pages/manage-book/manage-book.component.ts
+++ bound-frontend/src/app/modules/book/pages/manage-book/manage-book.component.ts
@@ -1,5 +1,5 @@
- import { Component } from '@angular/core';
- import { Router } from "@angular/router";
+ import { Component, OnInit } from '@angular/core';
+ import {ActivatedRoute, Router } from "@angular/router";
  import { BookRequest } from "src/app/services/models/book-request";
  import { BookService } from "src/app/services/services";
 
@@ -8,7 +8,7 @@
   templateUrl: './manage-book.component.html',
   styleUrls: ['./manage-book.component.scss']
 })
-export class ManageBookComponent {
+export class ManageBookComponent implements OnInit {
   errorMessage: Array<string> = [];
   selectedPicture: string | undefined;
   selectedBookCover: any;
@@ -16,8 +16,33 @@ export class ManageBookComponent {
 
   constructor(
     private bookService: BookService,
-    private router: Router
+    private router: Router,
+    private activatedRoute: ActivatedRoute
   ) {
+  }
+
+  ngOnInit(): void {
+    const bookId = this.activatedRoute.snapshot.params['bookId'];
+    if (bookId) {
+      this.bookService.getBookById({
+        'book-id': bookId
+      })
+      .subscribe({
+        next: book => {
+          this.bookRequest = {
+            id: book.id,
+            title: book.title as string,
+            authorName: book.authorName as string,
+            isbn: book.isbn as string,
+            synopsis: book.synopsis as string,
+            shareable: book.shareable
+          }
+          if (book.bookCover) {
+            this.selectedPicture = 'data:image/jpeg;base64,' + book.bookCover;
+          }
+        }
+      })
+    }
   }
 
   protected onFileSelected(event: any) {
